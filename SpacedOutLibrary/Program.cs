d3991c0a3395496934324f0fe7e2adacb32c645c
Your Name <n8jefferis@gmail.com>
2019-05-10 08:58:56 -0400

    fixed book listview

--- SpacedOutLibrary/Program.cs
+++ SpacedOutLibrary/Program.cs
@@ -10,25 +10,39 @@ namespace SpacedOutLibrary
     {
         static void Main(string[] args)
         {
-            Book b = new Book();
+            
 
-            b.Title = "Star Wars";
-            b.Author = "George Lucas";
-            b.Status = false;
-
-            DateTime dat = new DateTime();
-            b.DueDate = dat.ToString("01/01/1900");
-            Console.Write(b.DueDate);
+            List<string> book = new List<string>() {"Farenheit 451", "Ray Radbury", "Dune",
+                "Frank Herbert", "The Callista Chronicles", "Callista Gloss", "Frankenstein", "Mary Shelly",
+            "A Wrinkle in Time", "Madeleine L'Engle", "2001", "Arthur C. Clarke",
+                "The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "Star Wars", "George Lucas",
+            "I,Robot","Issac Asimov", "Jurassic Park", "Michael Crichton", "1984", "George Orwell", "Avengers End Game", "Stan Lee",
+            };
 
             List<Book> Books = new List<Book>();
-            Books.Add(b);
-           
+
+            for(int i = 0; i < book.Count; i+=2)
+            {
+                Book b = new Book();
+
+                b.Title = book[i];
+          
+                b.Author = book[i+1];
+             
+                b.Status = false;
+                b.DueDate = "";
+
+                Books.Add(b);
+                
+            }
+
+
 
             BookListView blv = new BookListView(Books);
 
             blv.Display();
 
-            
+
 
 
 
