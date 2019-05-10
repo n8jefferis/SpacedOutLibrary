08d3d10861a7003c7a860a5d8c0acc28665835cf
Your Name <n8jefferis@gmail.com>
2019-05-10 18:07:04 -0400

    added things

--- SpacedOutLibrary/BookView.cs
+++ SpacedOutLibrary/BookView.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SpacedOutLibrary
 {
-    class BookView
+    public class BookView
     {
         //props
         public Book Displaybook { get; set; }
@@ -17,16 +17,11 @@ namespace SpacedOutLibrary
             this.Displaybook = DisplayBook;
         }
 
+
         //meth
-        public void View()
-        {
-            Console.WriteLine("Here is your book's information: ");
-            Console.WriteLine();
-            Console.WriteLine($"Title: {Displaybook.Title}");
-            Console.WriteLine($"Author: {Displaybook.Author}");
-            Console.WriteLine($"Status: {(Displaybook.Status == true ? "CHECKED OUT" : "Available" )}");
-            Console.WriteLine($"Due Date: {(Displaybook.Status == false ? "" : Displaybook.DueDate.ToString() )}");
-        }
+        
+
+
         //public string DateSplit(DateTime )
         //{
         //    string temp = DueDate.ToString();
